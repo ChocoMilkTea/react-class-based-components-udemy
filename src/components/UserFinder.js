cb4458fb8adb7be3cc19e360543aebd9d1af8130
ChocoMilkTea <hongjun87@gmail.com>
2021-06-02 22:34:11 +0800

    update UserFinder to class based component

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Component, Fragment } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -9,29 +9,47 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const UserFinder = () => {
+class UserFinder extends Component {
 
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-    );
-  }, [searchTerm]);
-
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value);
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: ''
+    };
   }
 
-  return (
-    <Fragment>
-      <div className={classes.finder}>
-        <input type='search' className={classes.finder} onChange={searchChangeHandler} />
-      </div>
-      <Users users={filteredUsers} />
-    </Fragment>
-  )
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  // componentDidMount() {
+  //   Send http request..
+  //   Render for the first time
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) => (
+          user.name.includes(this.state.searchTerm)
+        ))
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' className={classes.finder} onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    )
+  }
 }
 
 export default UserFinder;
